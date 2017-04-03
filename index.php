3e9b604eeb55d21f314c513b7391bdade47e21ef
jpdamiani <joaopfdamiani@gmail.com>
2017-04-02 23:53:38 -0300

    metodos de lista

--- index.php
+++ index.php
@@ -3,19 +3,32 @@
 	require_once("config.php");
 
 	//$sql = new Sql();
-
-
-
 	//$usuarios = $sql->select("SELECT * FROM tb_usuarios");
-
-
 	//echo json_encode($usuarios);
 
 
-	$root = new Usuario();
+	//carrega um usuario
+	//$root = new Usuario();
+	//$root->loadById(1);
+	//echo $root;
 
-	$root->loadById(1);
 
-	echo $root;
+// carrega varios usuarios
+	//$lista = Usuario::getList();
+	//echo json_encode($lista);
+
+
+
+//carrega uma lista de usu pelo login
+	//$search = Usuario::search("ro");
+	//echo json_encode($search)
+
+
+
+$usuario = new Usuario();
+$usuario->login("jose","1233242342");
+
+
+echo $usuario;
 
  ?>
